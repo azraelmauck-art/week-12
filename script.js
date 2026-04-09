16e5f6cd7ae37fa0c5073eddf3c082a8b766c815
azraelmauck-art <azraelmauck@gmail.com>
2026-04-09 16:19:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-const gallery = document.getElementById("gallery"); const box = document.querySelector(".box");
-const colorButton = document.getElementById("changeColor"); const toggleButton = document.getElementById("toggleImage");
-colorButton.addEventListener("click", ()
-box.style.backgroundColor =
-=> {
-box.style.backgroundColor === "blue" ? "red" : "blue";
-  colorButton.addEventListener("click", () => {
-    bx.style.backgroundColor = "blue"; 
+  const gallery = document.getElementById("gallery");
+  const box = document.getElementById(".box");
+  const colorButton = document.getElementById("changeColor");
+  const toggleButton = document.getElementById("toggleImage");
+  
+  let toggled = false;
+  toggleButton.addEventListener("click", () => {
+    if (toggled) {
+      gallery.src = "./assets/images/image1.png";
+      toggled = false;
+    } else {
+      gallery.src = "./assets/images/pumpkin.png";
+      toggled = true;
+    }
   });
- 
-let toggled = false;
-toggleButton.addEventListener("click", () => {
-if (toggled) {
-gallery.src = "./assets/images/image1.png";
-toggled = false;
-} else {
-gallery.src = "./assets/images/pumpkin.png"; // wrong filename
-toggled = true;
-console.log("Page loaded!"');
+  
+  console.log("Page loaded!");
+});
